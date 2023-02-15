354029f3c02b1c95d1660ef7a1623a9eb69d9cea
Dhruv Maheshwari <mrdm026@gmail.com>
2023-02-15 23:16:40 +0530

    inital files added

---
+++ src/main/java/com/training/grpcjava/GRpcJavaApplication.java
@@ -0,0 +1,13 @@
+package com.training.grpcjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GRpcJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/grpcjava/GRpcJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.grpcjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
